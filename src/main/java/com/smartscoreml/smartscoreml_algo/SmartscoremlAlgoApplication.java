5b7eae2e15f94f0bf3ee0eaa080c268fce2ebbad
xcutiepatootie <mackycalabia@gmail.com>
2024-03-02 18:00:13 +0800

    initial commit

---
+++ src/main/java/com/smartscoreml/smartscoreml_algo/SmartscoremlAlgoApplication.java
@@ -0,0 +1,13 @@
+package com.smartscoreml.smartscoreml_algo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartscoremlAlgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartscoremlAlgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartscoreml/smartscoreml_algo/SmartscoremlAlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartscoreml.smartscoreml_algo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartscoremlAlgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
