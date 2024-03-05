a1d30b4ea94bb80958c2ce2140769a904c7cc6d8
xcutiepatootie <mackycalabia@gmail.com>
2024-03-05 22:50:37 +0800

    feat: updated controllers

--- src/main/java/com/smartscoreml/smartscoreml_algo/controller/ChartController.java
+++ src/main/java/com/smartscoreml/smartscoreml_algo/controller/ChartController.java
@@ -43,36 +43,29 @@ public class ChartController {
     }
 
     private byte[] generateClusterPlot(Instances data, int[] assignments, SimpleKMeans kMeans) {
-        // Your existing plotClusters logic here
+        // Create dataset
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        XYSeries[] series = new XYSeries[kMeans.getNumClusters()];
+        for (int i = 0; i < series.length; i++) {
+            series[i] = new XYSeries("Cluster " + (i + 1));
+        }
+        for (int i = 0; i < data.numInstances(); i++) {
+            series[assignments[i]].add(data.instance(i).value(0), data.instance(i).value(1));
+        }
+        for (int i = 0; i < series.length; i++) {
+            dataset.addSeries(series[i]);
+        }
 
-            // Create dataset
-            XYSeriesCollection dataset = new XYSeriesCollection();
-            XYSeries[] series = new XYSeries[kMeans.getNumClusters()];
-            for (int i = 0; i < series.length; i++) {
-                series[i] = new XYSeries("Cluster " + (i + 1));
-            }
-            for (int i = 0; i < data.numInstances(); i++) {
-                series[assignments[i]].add(data.instance(i).value(0), data.instance(i).value(1));
-            }
-            for (int i = 0; i < series.length; i++) {
-                dataset.addSeries(series[i]);
-            }
-
-            // Add centroids
-            Instances centroids = kMeans.getClusterCentroids();
-            XYSeries centroidSeries = new XYSeries("Centroids");
-            for (int i = 0; i < centroids.numInstances(); i++) {
-                centroidSeries.add(centroids.instance(i).value(0), centroids.instance(i).value(1));
-            }
-            dataset.addSeries(centroidSeries);
-
-        // Instead of creating a JFrame and displaying the chart,
-        // Create the chart and save it as an image
-
-        // Create dataset, create chart, etc. (same as in plotClusters method)
+        // Add centroids
+        Instances centroids = kMeans.getClusterCentroids();
+        XYSeries centroidSeries = new XYSeries("Centroids");
+        for (int i = 0; i < centroids.numInstances(); i++) {
+            centroidSeries.add(centroids.instance(i).value(0), centroids.instance(i).value(1));
+        }
+        dataset.addSeries(centroidSeries);
 
         // Create chart
-        JFreeChart chart = ChartFactory.createScatterPlot("Cluster Plot", "Score", "Time", dataset);
+        JFreeChart chart = ChartFactory.createScatterPlot("Cluster Plot", "X", "Y", dataset);
 
         // Convert chart to image bytes
         byte[] imageBytes = null;
--- src/main/java/com/smartscoreml/smartscoreml_algo/controller/SmartScoreController.java
+++ src/main/java/com/smartscoreml/smartscoreml_algo/controller/SmartScoreController.java
@@ -1,9 +1,17 @@
 package com.smartscoreml.smartscoreml_algo.controller;
 
+import com.smartscoreml.smartscoreml_algo.model.StudentClusterModel;
 import com.smartscoreml.smartscoreml_algo.service.ClusteringService;
+import com.smartscoreml.smartscoreml_algo.service.WekaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import weka.core.Instances;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -11,8 +19,22 @@ public class SmartScoreController {
     @Autowired
     private ClusteringService clusteringService;
 
-  /*  @GetMapping("/assignments")
-    public List<StudentClusterModel> getClusterAssignments() throws Exception{
+    @Autowired
+    private WekaService wekaService;
 
-    }*/
+
+    @GetMapping("/assignments")
+    public List<StudentClusterModel> getClusterAssignments() throws Exception{
+        Instances data = clusteringService.loadData(); // Implement loadData method as needed
+        int[] assignments = clusteringService.getClusterAssignments(); // Implement buildClustererAndGetAssignments method as needed
+
+        List<StudentClusterModel> studentClusters = new ArrayList<>();
+        Map<String, Integer> studentClusterMap = clusteringService.createStudentClusterMap(data, assignments);
+
+        for (Map.Entry<String, Integer> entry : studentClusterMap.entrySet()) {
+            studentClusters.add(new StudentClusterModel(entry.getKey(), entry.getValue()));
+        }
+
+        return studentClusters;
+    }
 }
